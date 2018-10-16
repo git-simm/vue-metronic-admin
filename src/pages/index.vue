ebac37390dee7ca099b555a7691a599e61bd2853
manman.si <214382122@qq.com>
2018-10-16 21:07:49 +0800

    添加vue-resource 服务请求组件

---
+++ src/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="container">
+		<h1 class="footer" @click="show()">test footer test footer</h1>
+		<input type="text" v-model="myMsg">
+		<div>{{ myMsg }}</div>
+    <p v-for="m in menus" :key="m">{{m.name}}</p>
+	</div>
+</template>
+<script>
+
+export default {
+  name: 'tq-footer',
+	props:['msg'],
+	//数据
+	data: function() {
+		return {
+			name : '这里是tq-footer组件，谢谢!',
+      myMsg : this.msg,
+      menus :[]
+		}
+  },
+  mounted:function(){
+    //编译完成后执行
+    this.getMenus();
+  },
+	//方法
+	methods: {
+		show: function() {
+			var i=0;
+			alert(this.name);
+    },
+    getMenus:function(){
+       var _this = this;
+       this.$http.get('static/data/menu.json',null).then(
+         function(res){
+           _this.menus = res.body;
+         }
+       );
+    }
+	}
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: red;
+}
+.container{
+	background-color: aliceblue;
+	padding: 10px;
+	margin: 10px 0;
+}
+.footer{
+	height: 50px;
+	background-color: lawngreen;
+}
+input{
+	width:400px;
+	border: 2px solid bisque;
+	border-radius: 4px;
+	height: 30px;
+	margin: 5px 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
+import vueResource from 'vue-resource'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
-
+Vue.use(vueResource);
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path:'/index',
+      name:'index',
+      component:()=>import('@/pages/index.vue')
     }
   ]
 })
