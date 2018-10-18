b0eb53162e3dd0f43d1b637fe81c002a537d9838
manman.si <214382122@qq.com>
2018-10-18 15:42:04 +0800

    s
    sdf

--- src/components/common/Content.vue
+++ src/components/common/Content.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="content">
-      <p>{{ comment }}</p>
-      <div class="next"><slot></slot></div>
-    </div>
+  <div class="content">
+    <p>{{ comment }}</p>
+    <div class="next"><slot>{{ comment+'的评论区' }}</slot></div>
+  </div>
 </template>
 <script>
 let timer = null
 export default {
   name: 'Content',
-  props: ['comment']
+  props: {
+    comment:{
+      type:String,
+      default:'无评论'
+    }
+  }
 }
 </script>
 <style scoped>
@@ -21,6 +26,6 @@ export default {
  .content{
    background-color:#333;
    color: burlywood;
-   height: 40px;
+   border-radius: 4px;
  }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,6 +7,7 @@
          </Content>
        </Content>
     </Content>
+    <Content/>
 	</div>
 </template>
 <script>
