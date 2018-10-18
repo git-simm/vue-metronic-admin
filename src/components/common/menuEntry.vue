5ded09945137a77822648610c798911c106e2851
yang.shang <Qaz2810714>
2018-10-18 15:09:27 +0800

    修改菜单栏

--- src/components/common/menuEntry.vue
+++ src/components/common/menuEntry.vue
@@ -14,26 +14,14 @@
 
                 </a>
 
-                <ul v-if="m.id !=null" class="sub-menu">
+                <ul class="sub-menu">
                     <li v-for="m2 in m.children" :key="m2.id">
-
-                            <a>
                                 <router-link :to="m2.url">
                                 {{m2.name}}
                                 </router-link>
-                                </a>
                     </li>
                 </ul>
 
-                <ul v-else class="page-sidebar-menu">
-                    <li v-for="m2 in m.children" v-bind:todo="m2" v-bind:key="m2.id">
-
-                        <a href="layout_horizontal_sidebar_menu.html">
-
-                            {{m2.name}}</a>
-
-                    </li>
-                </ul>
 
             </li>
         </ul>
