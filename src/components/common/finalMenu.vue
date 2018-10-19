29a15f8156bf935ab797aad46b7cbfe272a4e6cb
manman.si <214382122@qq.com>
2018-10-19 18:26:23 +0800

    3
    3

--- src/components/common/finalMenu.vue
+++ src/components/common/finalMenu.vue
@@ -1,8 +1,13 @@
 <template>
   <!-- <div> -->
   <li>
-      <router-link :to="menu.url">{{menu.name}} </router-link>
-      <ul class="sub-menu" v-show=" menu.children!=null && menu.children.lenght>0 ">
+      <router-link :to="menu.url" v-if=" !hasChild ">{{menu.name}}</router-link>
+      <a href="javascript:;" v-if="hasChild">
+          <i class="icon-cogs"></i>
+          <span class="title">{{menu.name}}</span>
+          <span class="arrow "></span>
+      </a>
+      <ul class="sub-menu" v-if=" hasChild">
           <finalMenu v-for="m in menu.children" :key="m.id" :menu=m></finalMenu>
       </ul>
   </li>
@@ -12,6 +17,11 @@
 let timer = null
 export default {
   name: 'finalMenu',
+  data : function(){
+    return {
+      hasChild: true
+    }
+  },
   props:{
     menu:{
       type: Object,
@@ -19,6 +29,9 @@ export default {
         return {};
       }
     }
+  },
+  mounted:function(){
+     this.hasChild = this.menu.children!=undefined && this.menu.children.length>0;
   }
 }
 </script>
